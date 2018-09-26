f93890e2f9a66ccad97a343acd036e3f150aa277
Mark Petruska <petruska.mark@gmail.com>
2018-09-26 06:58:09 +0200

    fixes installation process for local files

--- install.sh
+++ install.sh
@@ -7,12 +7,16 @@ pushd $HOME
 for file in $(find $SOURCE_DIR \
                 -type f \
                 -not -path "*/.git/*" \
-                -not -path "*/.gitignore" \
+                -not -path "*/.bashrc_local" \
+                -not -path "*/.ssh/config_local" \
                 -not -path "*/install.sh")
 do
   TARGET=$(realpath --relative-base="$SOURCE_DIR" "$file")
   mkdir -p "$(dirname "$TARGET")"
   ln -s -v $file $TARGET
 done
+
+cp -n -v $SOURCE_DIR/.bashrc_local .
+cp -n -v $SOURCE_DIR/.ssh/config_local .ssh/
 
 popd
