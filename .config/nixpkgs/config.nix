3088e08f85cddded8342ed9300d32a2892516d8e
Mark Petruska <petruska.mark@gmail.com>
2018-10-07 22:59:59 +0200

    configures oracle jdk

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,3 +1,23 @@
+
 {
+
   allowUnfree = true;
+
+  packageOverrides = pkgs: rec {
+  
+    sbt = pkgs.sbt.override {
+      jre = pkgs.oraclejdk8;
+    };
+
+    scala = pkgs.scala.override {
+      jre = pkgs.oraclejdk8;
+    };
+
+#   Idea 2018.2.4 does not like this.
+#    jetbrains.idea-community = pkgs.jetbrains.idea-community.override {
+#      jdk = pkgs.oraclejdk8;
+#    };
+
+  };
+  
 }
--- nix-packages.sh
+++ nix-packages.sh
@@ -1,3 +1,4 @@
+
 #/bin/bash
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
@@ -7,10 +8,10 @@ nix-channel --update
 
 nix-env -iA nixos.firefox nixos.termite nixos.ranger nixos.slack \
 	nixos.xorg.xkill nixos.skype nixos.dpkg nixos.openvpn \
-	nixos.chromium
+	nixos.chromium nixos.lm_sensors
 
 # Development
 
-nix-env -iA nixos.atom nixos.docker nixos.git nixos.jdk nixos.sbt nixos.scala \
-        nixos.emacs nixos.jetbrains.idea-community nixos.vagrant \
+nix-env -iA nixos.atom nixos.docker nixos.git nixos.sbt nixos.scala \
+	nixos.emacs nixos.jetbrains.idea-community nixos.vagrant \
 	nixos.dbeaver
