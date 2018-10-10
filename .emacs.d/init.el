4d55a06978f401d84d8239e981503cd18990d620
Mark Petruska <petruska.mark@gmail.com>
2018-10-10 13:04:41 +0200

    fine tunes emacs font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,6 @@
 
+
+
 (defconst user-init-dir
   (cond ((boundp 'user-emacs-directory)
 	  user-emacs-directory)
@@ -26,7 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Code" :foundry "CYRE" :slant normal :weight normal :height 80 :width normal)))))
+ '(default ((t (:family "Fira Code" :foundry "CYRE" :slant normal :weight normal :height 77 :width normal)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
