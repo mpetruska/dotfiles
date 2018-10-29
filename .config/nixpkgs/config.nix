2a581ad664e7eacae5e516b846f0428d0066d66c
Mark Petruska <petruska.mark@gmail.com>
2018-10-29 12:17:27 +0100

    removes irrelevant section

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -13,11 +13,6 @@
       jre = pkgs.oraclejdk8;
     };
 
-#   Idea 2018.2.4 does not like this.
-#    jetbrains.idea-community = pkgs.jetbrains.idea-community.override {
-#      jdk = pkgs.oraclejdk8;
-#    };
-
   };
   
 }
