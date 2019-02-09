f76935421cdcc2e4b9aeb7d4f02c4f072814b1ea
Mark Petruska <petruska.mark@gmail.com>
2019-02-09 09:20:16 +0100

    further packages and tweaks

--- nix-packages.sh
+++ nix-packages.sh
@@ -9,7 +9,7 @@ nix-env -iA nixos.firefox nixos.termite nixos.ranger nixos.slack \
 	nixos.xorg.xkill nixos.skype nixos.dpkg nixos.openvpn \
 	nixos.lm_sensors nixos.postman nixos.kdeApplications.spectacle \
 	nixos.calc nixos.libreoffice nixos.unzip nixos.lolcat \
-	nixos.cowsay
+	nixos.cowsay nixos.conkeror
 
 # Media
 
@@ -22,4 +22,4 @@ nix-env -iA nixos.mplayer nixos.youtubeDL nixos.python36Packages.mps-youtube \
 nix-env -iA nixos.atom nixos.docker nixos.git nixos.sbt nixos.scala \
 	nixos.emacs nixos.jetbrains.idea-community nixos.vagrant \
 	nixos.dbeaver nixos.nodejs nixos.yarn nixos.figlet nixos.gnumake \
-	nixos.jq
+	nixos.jq nixos.telnet
