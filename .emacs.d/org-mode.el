c852a390a540fa6e90c080957a6b113f1103a580
Mark Petruska <petruska.mark@gmail.com>
2019-04-01 22:04:15 +0200

    sets up org agenda

--- .emacs.d/org-mode.el
+++ .emacs.d/org-mode.el
@@ -3,7 +3,10 @@
   :ensure org-plus-contrib
   :config
   (progn
+    (global-set-key (kbd "C-c a") 'org-agenda)
     (setq org-hide-leading-stars t)
+    (setq org-agenda-files (list "~/GitLab/org"))
+    (setq org-archive-location "~/GitLab/org/archive.org::")
   ))
 
 (use-package org-bullets
