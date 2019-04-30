50fab7fba66f0ef68d410d6f8193bbdc765af653
Mark Petruska <petruska.mark@gmail.com>
2019-04-30 06:20:52 +0200

    tweaks /etc/nixos/configuration.nix

--- root/etc/nixos/configuration.nix
+++ root/etc/nixos/configuration.nix
@@ -12,6 +12,9 @@
   # Boot settings.
   boot = {
 
+    # some driver issues with kernel 4.19
+    kernelPackages = pkgs.linuxPackages_4_14;
+
     initrd = {
       # Disable journaling check on boot because virtualbox doesn't need it
       # checkJournalingFS = false;
