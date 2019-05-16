029fe7e70fb4534d5826daf39304d7b25bae89fa
Mark Petruska <petruska.mark@gmail.com>
2019-05-16 19:50:28 +0200

    tweaks

--- nix-packages.sh
+++ nix-packages.sh
@@ -17,7 +17,7 @@ nix-env -iA nixos.firefox nixos.termite nixos.ranger nixos.slack \
 
 nix-env -iA nixos.mplayer nixos.youtubeDL nixos.python36Packages.mps-youtube \
 	nixos.asciinema nixos.w3m nixos.libcaca nixos.vokoscreen nixos.espeak \
-	nixos.cmus nixos.powerline-fonts nixos.tetex
+	nixos.cmus nixos.powerline-fonts nixos.tetex nixos.lsof
 
 # Development
 
