3a895188477239a5b9ae3f697b853749d3f7efd9
Mark Petruska <petruska.mark@gmail.com>
2020-04-03 11:26:56 +0200

    tweaks projectile and bloop

--- .emacs.d/projectile.el
+++ .emacs.d/projectile.el
@@ -3,7 +3,14 @@
   :ensure t
   :pin melpa-stable
   :config
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1)
-  (setq projectile-project-search-path '("~/GitHub/" "~/Wst/GitLab/")))
+    (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+    (projectile-mode +1)
+    (setq projectile-project-search-path '("~/GitHub/" "~/Wst/GitLab/"))
+    (projectile-register-project-type 'bloop '(".bloop")
+                                  :compile "bloop compile --reporter scalac --no-color root"
+                                  :test "bloop test --propagate --reporter scalac --no-color root"
+                                  :src-dir "src/main/"
+                                  :test-dir "src/test/"
+                                  :test-suffix "Spec")
+)
